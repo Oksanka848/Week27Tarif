a78d9b0b042c9ae3da36012fa3a5c6ffe4b3c1c3
Oksanaka848 <okmitka@mail.ru>
2023-02-06 22:24:35 +0300

    ch

---
+++ week27tarif/src/components/Tarif.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Tarif() {
+  return (
+    <>
+    <div className='card_wrapper'>
+      <div className='card_component'>
+      <div className='card_component__item-1'></div>
+      <div className='card_component__item-2'></div>
+      <div className='card_component__item-3'></div>
+      </div>
+        <div className='card_footer'>
+      </div>
+      </div>
+        </>
+  )
+}
