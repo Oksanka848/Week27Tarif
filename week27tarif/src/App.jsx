8fab77fbd53440cd042068d003d85b5591fe773c
Oksanaka848 <okmitka@mail.ru>
2023-02-08 22:58:21 +0300

    ch

--- week27tarif/src/App.jsx
+++ week27tarif/src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
-import './components/Tarif.scss';
+import './components/_variables.scss'
+import './components/Tarif.scss'
 import Tarif from './components/Tarif';
 import cards from './components/cards.json';
 
 export default class App extends React.Component {
     render() {
     return (
-      <div className="App">
+      <div className='App'>
         {
           cards.map((card) =>
             <Tarif key={card.id} price={card.price} speed={card.speed}></Tarif>
--- week27tarif/src/components/Tarif.jsx
+++ week27tarif/src/components/Tarif.jsx
@@ -1,10 +1,16 @@
-export default function Tarif(props) {
+import React from 'react';
+
+export default class Tarif extends React.Component {
+  render() {
+    const {price, speed} = this.props;
+    
   return (
     <div className='tarifcard'>
-      <div className='card__component header'><span className='tarif-name'>Безлимитный</span><p className='tarif-price'>{props.price}</p></div>
-      <div className='card__component item-1'><p className='tarif-cur'>руб</p><div className='tarif-price'>{props.price}</div><span>/мес</span></div>
-      <div className='card__component item-2'><span>До</span><div className='tarif-speed'>{props.speed}</div><span>Мбит/сек</span></div>
+      <div className='card__component header'><span>Безлимитный</span><p className='tarif-price'>{price}</p></div>
+      <div className='card__component item-1'><span className='tarif-cur'>руб</span><div className='tarif-price'>{price}</div><span>/мес</span></div>
+      <div className='card__component item-2'><span>до</span><div className='tarif-speed'>{speed}</div><span>Мбит/сек</span></div>
       <div className='card__component footer'><span>Объем ключенного трафика не ограничен</span></div>
       </div>
   );
+}
 }
