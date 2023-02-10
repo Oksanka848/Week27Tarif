85e7cf1cf4e705e078d77d39d3c6a5126af45325
Oksanaka848 <okmitka@mail.ru>
2023-02-10 22:12:26 +0300

    end

--- week27tarif/src/App.jsx
+++ week27tarif/src/App.jsx
@@ -1,18 +1,15 @@
 import React from 'react';
 import style from './App.css';
-
-/*import tarif from './components/Tarif.scss';*/
 import Tarif from './components/Tarif';
 import cards from './components/cards.json';
 
 export default class App extends React.Component {
-    render() {
+  render() {
     return (
       <div className={style.App}>
         {
           cards.map((card) =>
-            <Tarif key={card.id} price={card.price} speed={card.speed} isSelected={card.isSelected} isBlue={card.isBlue} isFiltr={card.isFiltr}
-            ></Tarif>
+            <Tarif key={card.id} price={card.price} speed={card.speed} isSelected={card.isSelected} theme={card.theme} isFiltr={card.isFiltr}></Tarif>
           )
         }
       </div>
